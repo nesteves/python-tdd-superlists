695ade293613d8e201fbf38170f45135af6e9f9b
Nuno Esteves <nesteves@users.noreply.github.com>
2016-01-03 22:45:22 +0000

    New URL and view for adding items to existing lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from lists import views
+from lists import views as list_views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/new$', list_views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', list_views.add_item, name='add_item'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
